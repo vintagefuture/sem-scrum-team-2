fcef082b0f11a23b730a00f97c3dee72aea3baf6
Salis <m.salis@go-jek.com>
2024-03-17 23:51:48 +0700

    fix database connection

--- src/main/java/com/napier/sem/DatabaseConnection.java
+++ src/main/java/com/napier/sem/DatabaseConnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://db:3306/world?useSSL=false";
-    private static final String USER = "root";
-    private static final String PASS = "example";
+    private static final String USER = "report";
+    private static final String PASS = "reportpassword";
 
     public static Connection getConnection() {
         try {
