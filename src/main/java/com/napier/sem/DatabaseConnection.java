57174a908d2a9972206ae8bdcd3a4fd7bcbf5d51
vintagefuture <pinkpallyn@gmail.com>
2024-04-02 10:36:25 +0100

    Fixing db connection
    Change-Id: I3d24b7386c0051fad10ca95e19dff55937cc8acd

--- src/main/java/com/napier/sem/DatabaseConnection.java
+++ src/main/java/com/napier/sem/DatabaseConnection.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/world?useSSL=false";
+    private static final String DB_URL = "jdbc:mysql://db:3306/world?useSSL=false";
     private static final String USER = "root";
     private static final String PASS = "example";
 
