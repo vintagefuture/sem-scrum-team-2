656de5761c48ce8a80c47a2571634c20ec1c2d6c
vintagefuture <pinkpallyn@gmail.com>
2024-04-20 16:13:03 +0100

    Updates to pom.xml
    Change-Id: I423d98c53b968dc43ac6e219454532cd9fc1dd47

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,10 +1,14 @@
+/**
+ * Main entry point for the application.
+ * Contains the call to each report.
+ */
 package com.napier.sem;
 
 import java.sql.Connection;
 
 /**
- * Main entry point for the application
- * Contains the call to each report
+ * Main entry point for the application.
+ * Contains the call to each report.
  */
 public class App {
     public static void main(String[] args) {
@@ -19,112 +23,180 @@ public class App {
             DB_URL = args[0];
             DELAY = Integer.parseInt(args[1]);
         }
+
+        /**
+         * Connect to database with try catch control logic
+         */
         try (Connection con = DatabaseConnection.connect(DB_URL, DELAY)) {
             if (con != null) {
 
-                // All countries in the world organised by largest population to smallest
+                /**
+                 * All countries in the world organised by largest population to smallest.
+                 */
                 CountriesPopulationReport countriesPopulationReport = new CountriesPopulationReport(con);
                 countriesPopulationReport.getCountriesPopulationInTheWorldReport();
 
-                // All the countries in a continent organised by largest population to smallest
+                /**
+                 * All the countries in a continent organised by largest population to smallest.
+                 */
                 countriesPopulationReport.getCountriesPopulationInContinentReport("Asia");
 
-                // All the countries in a region organised by largest population to smallest
+                /**
+                 * All the countries in a region organised by largest population to smallest.
+                 */
                 countriesPopulationReport.getCountriesPopulationInRegionReport("North America");
 
-                // The top N populated countries in the world where N is provided by the user
+                /**
+                 * The top N populated countries in the world where N is provided by the user.
+                 */
                 TopCountriesPopulationReport topCountriesPopulationReport = new TopCountriesPopulationReport(con);
                 topCountriesPopulationReport.getTopPopulatedCountriesInTheWorld(5);
 
-                // The top N populated countries in a continent where N is provided by the user
+                /**
+                 * The top N populated countries in a continent where N is provided by the user.
+                 */
                 topCountriesPopulationReport.getTopPopulatedCountriesInContinent("Asia", 5);
 
-                // The top N populated countries in a region where N is provided by the user
+                /**
+                 * The top N populated countries in a region where N is provided by the user.
+                 */
                 topCountriesPopulationReport.getTopPopulatedCountriesInRegion("Southern and Central Asia", 5);
 
-                // All the cities in the world organised by largest population to smallest
+                /**
+                 * All the cities in the world organised by largest population to smallest.
+                 */
                 CitiesReport citiesReport = new CitiesReport(con);
                 citiesReport.getCitiesPopulationReportInTheWorld();
 
-                // All the cities in a continent organised by largest population to smallest
+                /**
+                 * All the cities in a continent organised by largest population to smallest.
+                 */
                 citiesReport.getCitiesPopulationInContinent("Africa");
 
-                // All the cities in a region organised by largest population to smallest
+                /**
+                 * All the cities in a region organised by largest population to smallest.
+                 */
                 citiesReport.getCitiesPopulationInRegion("Western Europe");
 
-                // All the cities in a country organised by largest population to smallest
+                /**
+                 * All the cities in a country organised by largest population to smallest.
+                 */
                 citiesReport.getCitiesPopulationReportInCountry("Spain");
 
-                // All the cities in a district organised by largest population to smallest
+                /**
+                 * All the cities in a district organised by largest population to smallest.
+                 */
                 citiesReport.getCitiesPopulationReportInDistrict("Buenos Aires");
 
 
-                // The top N populated cities in the world where N is provided by the user
+                /**
+                 * The top N populated cities in the world where N is provided by the user.
+                 */
                 TopCitiesPopulationReport topCitiesPopulationReport = new TopCitiesPopulationReport(con);
                 topCitiesPopulationReport.getTopPopulatedCitiesInTheWorld(5);
 
-                // The top N populated cities in a continent where N is provided by the user
+                /**
+                 * The top N populated cities in a continent where N is provided by the user.
+                 */
                 topCitiesPopulationReport.getTopPopulatedCitiesInTheContinent("Asia", 5);
 
-                // The top N populated cities in a region where N is provided by the user
+                /**
+                 * The top N populated cities in a region where N is provided by the user.
+                 */
                 topCitiesPopulationReport.getTopPopulatedCitiesInTheRegion("Central Africa", 5);
 
-                // The top N populated cities in a country where N is provided by the user
+                /**
+                 * The top N populated cities in a country where N is provided by the user.
+                 */
                 topCitiesPopulationReport.getTopPopulatedCitiesInTheCountry("Indonesia", 5);
 
-                // The top N populated cities in a district where N is provided by the user
+                /**
+                 * The top N populated cities in a district where N is provided by the user.
+                 */
                 topCitiesPopulationReport.getTopPopulatedCitiesInTheDistrict("California", 5);
 
-                // All the capital cities in the world organised by largest population to smallest
+                /**
+                 * All the capital cities in the world organised by largest population to smallest.
+                 */
                 CapitalCitiesPopulationReport capitalCitiesPopulationReport = new CapitalCitiesPopulationReport(con);
                 capitalCitiesPopulationReport.getCapitalCitiesReportOfTheWorld();
 
-                // All the capital cities in a continent organised by largest population to smallest
+                /**
+                 * All the capital cities in a continent organised by largest population to smallest.
+                 */
                 capitalCitiesPopulationReport.getCapitalCitiesReportOfContinent("Africa");
 
-                // All the capital cities in a region organised by largest to smallest
+                /**
+                 * All the capital cities in a region organised by largest to smallest.
+                 */
                 capitalCitiesPopulationReport.getCapitalCityReportOfRegion("Caribbean");
 
-                // The top N populated capital cities in the world where N is provided by the user
+                /**
+                 * The top N populated capital cities in the world where N is provided by the user.
+                 */
                 TopCapitalCitiesPopulationReport topCapitalCitiesPopulationReport = new TopCapitalCitiesPopulationReport(con);
                 topCapitalCitiesPopulationReport.getTopCapitalCitiesInTheWorldReport(5);
 
-                // The top N populated capital cities in a continent where N is provided by the user
+                /**
+                 * The top N populated capital cities in a continent where N is provided by the user.
+                 */
                 topCapitalCitiesPopulationReport.getTopCapitalCitiesInTheContinentReport(3, "Asia");
 
-                // The top N populated capital cities in a region where N is provided by the user
+                /**
+                 * The top N populated capital cities in a region where N is provided by the user.
+                 */
                 topCapitalCitiesPopulationReport.getTopCapitalCitiesInTheRegionReport(5, "North America");
 
-                // The population of people, people living in cities, and people not living in cities in each continent
+                /**
+                 * The population of people, people living in cities, and people not living in cities in each continent.
+                 */
                 PopulationReport citiesPopulationReport = new PopulationReport(con);
                 citiesPopulationReport.generatePopulationInCitiesVSNonCityByContinent();
 
-                // The population of people, people living in cities, and people not living in cities in each region
+                /**
+                 * The population of people, people living in cities, and people not living in cities in each region.
+                 */
                 citiesPopulationReport.generatePopulationInCitiesVSNonCityByRegion();
 
-                // The population of people, people living in cities, and people not living in cities in each country
+                /**
+                 * The population of people, people living in cities, and people not living in cities in each country.
+                 */
                 citiesPopulationReport.generatePopulationInCitiesVSNonCityByCountry();
 
-                // The population of the world
+                /**
+                 * The population of the world.
+                 */
                 TotalPopulationReport totalPopulationReport = new TotalPopulationReport(con);
                 totalPopulationReport.getTotalPopulationInWorld();
 
-                // The population of a continent
+                /**
+                 * The population of a continent.
+                 */
                 totalPopulationReport.getTotalPopulationInContinent("Europe");
 
-                // The population of a region
+                /**
+                 * The population of a region.
+                 */
                 totalPopulationReport.getTotalPopulationInRegion("North America");
 
-                // The population of a country
+                /**
+                 * The population of a country.
+                 */
                 totalPopulationReport.getTotalPopulationInCountry("France");
 
-                // The population of a district
+                /**
+                 * The population of a district.
+                 */
                 totalPopulationReport.getTotalPopulationInDistrict("Tasmania");
 
-                // The population of a city
+                /**
+                 * The population of a city.
+                 */
                 totalPopulationReport.getTotalPopulationInCity("Amsterdam");
 
-                // Total number of people, with percentage of world population, who speak Chinese, English, Hindi, Spanish, Arabic, from greatest number to smallest
+                /**
+                 * Total number of people, with percentage of world population, who speak Chinese, English, Hindi, Spanish, Arabic, from greatest number to smallest.
+                 */
 
 
                 DatabaseConnection.closeConnection();
